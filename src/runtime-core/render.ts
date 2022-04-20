ba5e8da543d3ad62dbc215b0e77c7c1118876fac
nannn <2337286751@qq.com>
2022-04-21 01:25:44 +0800

    实现ShapeFlags，通过为运算进行优化

--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,4 +1,5 @@
 import { isObject } from "../shared/index";
+import { ShapeFlags } from "../shared/ShapeFlags";
 import { createComponentInstance, setupComponent } from "./component";
 
 export function render(vnode, container) {
@@ -7,11 +8,16 @@ export function render(vnode, container) {
   patch(vnode, container);
 }
 function patch(vnode, container) {
+  // ShapeFlags 描述虚拟节点当前类型
+  // vnode -> flag
+  // element
   // 去处理组件
   // 判断是不是 element 类型
-  if (typeof vnode.type === "string") {
+  const { shapeFlag } = vnode;
+  if (shapeFlag & ShapeFlags.ELEMENT) {
     processElement(vnode, container);
-  } else if (isObject(vnode.type)) {
+    // STATEFUL_COMPONENT
+  } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
     processComponent(vnode, container);
   }
 }
@@ -45,8 +51,9 @@ function processElement(vnode: any, container: any) {
 function mountElement(vnode: any, container: any) {
   const el = (vnode.el = document.createElement(vnode.type));
   // string array
-  const { children } = vnode;
-  if (typeof children === "string") {
+  const { children, shapeFlag } = vnode;
+  // text_children
+  if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
     el.textContent = children;
     // props
     const { props } = vnode;
@@ -54,7 +61,8 @@ function mountElement(vnode: any, container: any) {
       const val = props[key];
       el.setAttribute(key, val);
     }
-  } else if (Array.isArray(children)) {
+    // array_children
+  } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
     // vnode
     mountChildren(vnode, el);
   }
