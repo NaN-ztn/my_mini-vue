b88cbdf2713ea180b90414a78ad3dd2759f5f13a
nannn <2337286751@qq.com>
2022-04-25 11:42:40 +0800

    实现getCurrentInstance

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -16,11 +16,13 @@ export function createComponentInstance(vnode) {
   component.emit = emit.bind(null, component) as any;
   return component;
 }
+
 export function setupComponent(instance) {
   initProps(instance, instance.vnode.props);
   initSlots(instance, instance.vnode.children);
   setupStatefulComponent(instance);
 }
+
 function setupStatefulComponent(instance: any) {
   const component = instance.type;
   instance.proxy = new Proxy({ _: instance }, PublicInstanceProxyHandlers);
@@ -29,10 +31,16 @@ function setupStatefulComponent(instance: any) {
     // function:render() 函数
     // Object:注入到组件上下文
     // 组件 proprs shallowReadOnly 浅只读
+
+    // 实现 getCurrentInstance，在 setup 执行前进行赋值
+    setCurrentInstance(instance);
     const setupResult = setup(shallowReadonly(instance.props), { emit: instance.emit });
+    // setup 执行完毕后清空
+    setCurrentInstance(null);
     handleSetupResult(instance, setupResult);
   }
 }
+
 function handleSetupResult(instance, setupResult: any) {
   // function: render() 函数
   // Object: 注入到组件上下文
@@ -41,8 +49,20 @@ function handleSetupResult(instance, setupResult: any) {
   }
   finishComponentSetup(instance);
 }
+
 function finishComponentSetup(instance: any) {
   const component = instance.type;
 
   instance.render = component.render;
+}
+
+let currentInstance = null;
+
+export function getCurrentInstance() {
+  return currentInstance;
+}
+
+// 好处：便于在调用栈中进行调试
+export function setCurrentInstance(instance: any) {
+  currentInstance = instance;
 }
