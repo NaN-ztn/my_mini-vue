47dac3084477c809babb395395052e73cb2fcfe1
nannn <2337286751@qq.com>
2022-05-02 04:56:02 +0800

    实现解析三种联合类型

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -9,27 +9,45 @@ const enum TagType {
 
 export function baseParse(content: string) {
   const context = createParseContext(content);
-  return createRoot(parseChildren(context));
+  return createRoot(parseChildren(context, []));
 }
 
-function parseChildren(context) {
+function parseChildren(context, ancestors) {
   const nodes: any = [];
-  let node;
+  while (!isEnd(context, ancestors)) {
+    let node;
+    const s = context.source;
+    if (s.startsWith("{{")) {
+      node = parseInterpolation(context);
+    } else if (s[0] === "<") {
+      // element 判断准则首位为 '<' 第二位为字母（忽略大小写）
+      if (/[a-z]/i.test(s[1])) {
+        node = parseElement(context, ancestors);
+      }
+    }
+
+    if (!node) {
+      node = parseText(context);
+    }
+    nodes.push(node);
+  }
+  return nodes;
+}
+
+function isEnd(context: any, ancestors) {
   const s = context.source;
-  if (s.startsWith("{{")) {
-    node = parseInterpolation(context);
-  } else if (s[0] === "<") {
-    // element 判断准则首位为 '<' 第二位为字母（忽略大小写）
-    if (/[a-z]/i.test(s[1])) {
-      node = parseElement(context);
+  // 2.当遇到结束标签时
+  if (s.startsWith(`</`)) {
+    // 栈的数据结构，从后往前遍历性能更好
+    for (let i = ancestors.length - 1; i >= 0; i--) {
+      const tag = ancestors[i].tag;
+      if (startWithEndTagOpen(s, tag)) {
+        return true;
+      }
     }
   }
-
-  if (!node) {
-    node = parseText(context);
-  }
-  nodes.push(node);
-  return nodes;
+  // 1.source 有值
+  return !s;
 }
 
 function parseInterpolation(context) {
@@ -74,11 +92,25 @@ function createRoot(children) {
     children,
   };
 }
-function parseElement(context: any) {
+function parseElement(context: any, ancestors) {
   // 1.解析 tag
-  const element = parseTag(context, TagType.Start);
-  parseTag(context, TagType.End);
+  const element: any = parseTag(context, TagType.Start);
+  // 收集标签
+  ancestors.push(element);
+  // element 子数组
+  element.children = parseChildren(context, ancestors);
+
+  if (startWithEndTagOpen(context.source, element.tag)) {
+    parseTag(context, TagType.End);
+  } else {
+    throw new Error(`缺少结束标签${element.tag}`);
+  }
+  ancestors.pop();
   return element;
+}
+
+function startWithEndTagOpen(source, tag) {
+  return source.startsWith("</") && source.slice(2, 2 + tag.length).toLowerCase() === tag;
 }
 
 function parseTag(context: any, type: TagType) {
@@ -93,26 +125,30 @@ function parseTag(context: any, type: TagType) {
 
   return {
     type: NodeTypes.ELEMENT,
-    content: {
-      type: NodeTypes.ELEMENT,
-      tag,
-    },
+    tag,
   };
 }
 function parseText(context: any): any {
   // 1.获取content
-  const content = parseTextData(context, context.source.length);
+
+  let endTokens = ["<", "{{"];
+  let endIndex = context.source.length;
+  for (let i = 0; i < endTokens.length; i++) {
+    const index = context.source.indexOf(endTokens[i]);
+    if (index !== -1 && endIndex > index) {
+      endIndex = index;
+    }
+  }
+
+  const content = parseTextData(context, endIndex);
 
   // 2.推进
   advanceBy(context, content.length);
   console.log(context.source);
 
   return {
-    type: NodeTypes.ELEMENT,
-    content: {
-      type: NodeTypes.TEXT,
-      content,
-    },
+    type: NodeTypes.TEXT,
+    content,
   };
 }
 function parseTextData(context: any, length) {
